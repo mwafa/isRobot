508c52d3e0d5965129314802625052aaf4026ec0
Peter Law <PeterJCLaw@gmail.com>
2016-10-11 22:00:15 +0100

    Reflow
    Change-Id: I49def325ddec5ee7e3021a06e6dbd1369f660e26

--- sr/robot/__init__.py
+++ sr/robot/__init__.py
@@ -1,3 +1,9 @@
 from .simulator import Simulator
 from .sim_robot import SimRobot, AlreadyHoldingSomethingException
-from .vision import MARKER_ARENA, MARKER_ROBOT, MARKER_PEDESTAL, MARKER_TOKEN
+from .vision import (
+    MARKER_ARENA,
+    MARKER_ROBOT,
+
+    MARKER_PEDESTAL,
+    MARKER_TOKEN,
+)
