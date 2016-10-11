a0f1b5640eda0a6dd0ae87cf3dabaf4af93cbc5f
Peter Law <PeterJCLaw@gmail.com>
2016-10-11 21:03:10 +0100

    Reflow
    Change-Id: I7e64ebc8e8da85a421a2ec4101a614e2ba629652

--- sr/robot/arenas/__init__.py
+++ sr/robot/arenas/__init__.py
@@ -3,4 +3,9 @@ from pirate_plunder_arena import PiratePlunderArena
 from ctf_arena import CTFArena
 from sunny_side_up_arena import SunnySideUpArena
 
-__all__ = ['pirate_plunder_arena', 'ctf_arena', 'sunny_side_up_arena', 'arena']
+__all__ = [
+    'pirate_plunder_arena',
+    'ctf_arena',
+    'sunny_side_up_arena',
+    'arena',
+]
