fd410817a4996bea4cea920f0accea07c6a02f95
maswafa <wafax.4@gmail.com>
2019-03-19 18:07:30 +0700

    perbaikan komentar

--- tugas1.py
+++ tugas1.py
@@ -2,22 +2,15 @@ from sr.robot import *
 
 import time
 
-# Tantangan Kelompok 9:
-# Mengambil token sesuai urutan tertentu dan mengumpulkanya ke tengah.
-
-
-
-SEARCHING, DRIVING, HOME = range(3)
+SEARCHING, DRIVING = range(2)
 
 WAKTU = time.time()
 
 FAKTOR = 1
 
-tokenKe = 1
-
-kontra = [10,17,24,3,13,20,27,6]
-
 R = Robot()
+
+tokenKe = 1
 
 token_filter = lambda m: m.info.marker_type == MARKER_TOKEN_A and m.info.offset == tokenKe
 
@@ -46,7 +39,7 @@ while True:
             print "Token sighted. {3}-{0} is {1}m away, bearing {2} degrees. " \
                   .format(m.info.offset, m.dist, m.rot_y, m.info.marker_type)
             state = DRIVING
-        turn(50*FAKTOR, 0.1)
+        turn(50*FAKTOR, 0.1) #Berputar kekiri jika sebelumnya token ada di kiri dan sebaliknya.
 
     elif state == DRIVING:
         # print "Aligning..."
@@ -59,7 +52,16 @@ while True:
             if m.dist < 0.4:
                 print "Found it!"
                 if R.grab():
-                    state = HOME
+                    print "Gotcha!"
+                    turn(-50, 0.5)
+                    drive(50, 1)
+                    R.release()
+                    drive(-50, 0.5)
+                    if tokenKe < 8:
+                        tokenKe += 1
+                    else:
+                        print(time.time()-WAKTU)
+                        exit() 
                 else:
                     print "Aww, I'm not close enough."
                 # exit()
@@ -67,7 +69,7 @@ while True:
             elif -15 <= m.rot_y <= 15:
                 print "Ah, that'll do."
                 drive(100, m.dist/2)
-                FAKTOR = 1 if m.rot_y > 0 else -1
+                FAKTOR = 1 if m.rot_y > 0 else -1 #Jika token hilang dari pandangan maka mengetahui tokena ada dikiri atau kanan.
 
             elif m.rot_y < -15:
                 print "Left a bit..."
@@ -76,38 +78,3 @@ while True:
             elif m.rot_y > 15:
                 print "Right a bit..."
                 turn (12.5, 0.1)
-
-    elif state == HOME:
-        tokens = filter(lambda x: x.info.marker_type == MARKER_ARENA and x.info.offset == kontra[tokenKe-1],R.see())
-        if len(tokens) == 0:
-            turn(50, 0.1)
-        else:
-            m = tokens[0]
-            jarak = 4.5 if tokenKe < 5 else 5.5
-            if m.dist < jarak:
-                    print "Found it!"
-                    if R.release():
-                        state = SEARCHING
-                        drive(-100, 1)
-                        if tokenKe < 8:
-                            tokenKe += 1
-                        else:
-                            print(time.time()-WAKTU)
-                            exit() 
-                    else:
-                        print "Aww, I'm not close enough."
-                    # exit()
-
-            elif -10 <= m.rot_y <= 10:
-                print "Ah, that'll do."
-                drive(100, 0.1)
-                FAKTOR = 1 if m.rot_y > 0 else -1
-
-            elif m.rot_y < -10:
-                print "Left a bit..."
-                turn(-12.5, 0.1)
-
-            elif m.rot_y > 10:
-                print "Right a bit..."
-                turn (12.5, 0.1)
-
